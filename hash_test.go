2b3b15b61dfe78c0fc5a142b0ac8d370e3454b57
52funny <wwq9977@gmail.com>
2023-11-04 12:43:00 +0800

    fix: update test

--- hash_test.go
+++ hash_test.go
@@ -3,47 +3,47 @@ package pikpakhash
 import "testing"
 
 func BenchmarkHashBuf128K(b *testing.B) {
-	ph := NewPikPakHashWithBuf(DefaultCalcRoutines, 1<<17)
+	ph := NewPikPakHash(DefaultCalcRoutines, 1<<17)
 	for i := 0; i < b.N; i++ {
 		ph.HashFromPath("./b")
 	}
 }
 
 func BenchmarkHashBuf256K(b *testing.B) {
-	ph := NewPikPakHashWithBuf(DefaultCalcRoutines, 1<<18)
+	ph := NewPikPakHash(DefaultCalcRoutines, 1<<18)
 	for i := 0; i < b.N; i++ {
 		ph.HashFromPath("./b")
 	}
 }
 func BenchmarkHashBuf512K(b *testing.B) {
-	ph := NewPikPakHashWithBuf(DefaultCalcRoutines, 1<<19)
+	ph := NewPikPakHash(DefaultCalcRoutines, 1<<19)
 	for i := 0; i < b.N; i++ {
 		ph.HashFromPath("./b")
 	}
 }
 
 func BenchmarkHashBuf1M(b *testing.B) {
-	ph := NewPikPakHashWithBuf(DefaultCalcRoutines, 1<<20)
+	ph := NewPikPakHash(DefaultCalcRoutines, 1<<20)
 	for i := 0; i < b.N; i++ {
 		ph.HashFromPath("./b")
 	}
 }
 
 func BenchmarkHashRoutine8(b *testing.B) {
-	ph := NewPikPakHash(8)
+	ph := NewPikPakHash(8, DefaultBufferSize)
 	for i := 0; i < b.N; i++ {
 		ph.HashFromPath("./b")
 	}
 }
 
 func BenchmarkHashRoutine12(b *testing.B) {
-	ph := NewPikPakHash(12)
+	ph := NewPikPakHash(12, DefaultBufferSize)
 	for i := 0; i < b.N; i++ {
 		ph.HashFromPath("./b")
 	}
 }
 func BenchmarkHashRoutine16(b *testing.B) {
-	ph := NewPikPakHash(16)
+	ph := NewPikPakHash(16, DefaultBufferSize)
 	for i := 0; i < b.N; i++ {
 		ph.HashFromPath("./b")
 	}
